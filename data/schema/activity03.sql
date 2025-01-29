f976fc77e8add77727fcb9c23fb944fc38d66d12
aidazylfo <aida.zylfo@ada.ac.uk>
2025-01-29 11:49:32 +0000

    Oooo

--- data/schema/activity03.sql
+++ data/schema/activity03.sql
@@ -10,12 +10,11 @@ INSERT INTO branch_sample (branchNo, street, city, postcode) VALUES
 ('B003', '09 Rruga Durrsit', 'Tiron', 'MH7 7IN');
 
 CREATE TABLE staff.sample(
-    staffNo TEXT UNIQUE CHECK (staffNo LIKE ' ')
+    staffNo TEXT UNIQUE CHECK (staffNo LIKE 'S[423-566]')
 )
 
 INSERT INTO staff.sample (staffNo, fName, lName, position, sex, DOB, salay, branchNo) VALUES 
 ('S432', 'Aida', 'Zylfo', 'CEO', 'F', '13-Jun-2006', '100000', 'B001')
-('S444', )
-('S501', )
-('S566', )
+('S444', 'Elez', 'Zylfo', 'CFO', 'M', '11-Apr-1935', '100000000000000000000000000', 'B003')
+('S501', 'Dennis', 'Hyka', 'Manager', 'M', '02-Aug-1974', '100000', 'B002' );
 
